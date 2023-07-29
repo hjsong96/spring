d0869563cf330163c4200203d7acc267eb24e97f
hjsong96 <ghkwlsdbwls@gmail.com>
2023-07-29 18:35:08 +0900

    spring

--- hi-spring/src/main/java/hi/hispring/controller/HiController.java
+++ hi-spring/src/main/java/hi/hispring/controller/HiController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HiController {
     @GetMapping("hi")
     public String hi(Model model) {
-        model.addAttribute("data", "spring!!");
+        model.addAttribute("data", "hi!!");
         return "hi";
     }
 }
